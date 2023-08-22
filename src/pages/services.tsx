bc12d0ca48ad303727f433d8951fe2c3b005fbaa
king danie <daniedemola@gmail.com>
2023-08-22 12:24:38 +0100

    added sevices page

---
+++ src/pages/services.tsx
@@ -0,0 +1,9 @@
+import Layout from '@/components/layout';
+
+export default function Services() {
+  return (
+    <Layout>
+      <div className="text-primary">This is the services page</div>
+    </Layout>
+  );
+}
