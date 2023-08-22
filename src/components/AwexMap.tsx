8a2aaa524606bc04f3fca0acdf24a80044028df0
king danie <daniedemola@gmail.com>
2023-08-22 12:23:27 +0100

    added awexmap comp

---
+++ src/components/AwexMap.tsx
@@ -0,0 +1,18 @@
+
+const AwexMap = () => {
+    return (
+      <div className="min-h-25vh">
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.111960256491!2d3.360206174495132!3d6.5075096233499865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8c4152cb60d3%3A0x47c27cbd3938e4a8!2s3%20Clegg%20Ln%2C%20Surulere%20101241%2C%20Lagos!5e0!3m2!1sen!2sng!4v1692628718174!5m2!1sen!2sng"
+          width="100%"
+          height="300px"
+          style={{ border: 0 }}
+          allowfullscreen=""
+          loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"
+        ></iframe>
+      </div>
+    );
+}
+
+export default AwexMap;
