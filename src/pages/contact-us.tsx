2b3aaf5733f67dba9e6a15a5a308e86dcb316c6d
king danie <daniedemola@gmail.com>
2023-08-22 12:24:57 +0100

    added contact page

---
+++ src/pages/contact-us.tsx
@@ -0,0 +1,9 @@
+import Layout from '@/components/layout';
+
+export default function ContactUs() {
+  return (
+    <Layout>
+      <div className="text-primary">This is the contact  page</div>
+    </Layout>
+  );
+}
