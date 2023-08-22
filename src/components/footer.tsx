5407051d5d6da9275b6d0e43a9253f210b01c0ba
king danie <daniedemola@gmail.com>
2023-08-22 12:22:34 +0100

    footer 90% done

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,31 +1,41 @@
 import React from 'react';
 // In your component or page
-import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+} from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-
-const navLinks = [
-  { name: 'Home', href: '/' },
-  { name: 'About', href: '/about-us' },
-  { name: 'Services', href: '/services' },
-  { name: 'Contact Us', href: '/contact-us' },
-];
+import { navLinks } from '@/utils/helperfunctions';
+import { useRouter } from 'next/router';
+import { EnvelopeIcon, MapIcon, PhoneIcon } from '@heroicons/react/24/outline';
+import Link from 'next/link';
+import AwexMap from './AwexMap';
 
 const Footer = () => {
+  const router = useRouter();
+
   return (
-    <footer className="w-full bg-accent">
-      <div className="min-h-25vh">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.111960256491!2d3.360206174495132!3d6.5075096233499865!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b8c4152cb60d3%3A0x47c27cbd3938e4a8!2s3%20Clegg%20Ln%2C%20Surulere%20101241%2C%20Lagos!5e0!3m2!1sen!2sng!4v1692628718174!5m2!1sen!2sng"
-          width="100%"
-          height="300px"
-          style={{ border: 0 }}
-          allowfullscreen=""
-          loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
-        ></iframe>
-      </div>
-      <div className="bg-black p-5 awe-nav-links flex flex-col items-center py-10 h-60">
+    <footer className="w-full bg-primary">
+      {/* <AwexMap /> */}
+      <div className="p-5 awe-nav-links flex flex-col items-center py-10 min-h-60">
+        <div className="sm:flex w-[90vw] justify-between items-center mb-10" style={{marginTop: "-5em"}}>
+          <div className="flex justify-center items-center gap-x-3 bg-accent w-full p-5">
+            <MapIcon className="block h-12 w-12" aria-hidden="true" />
+            <Link href="">3 Clegg Lane Ojuelegba – Surulere</Link>
+          </div>
+          <div className="flex justify-center items-center gap-x-3 bg-black w-full p-5">
+            <PhoneIcon className="block h-12 w-12" aria-hidden="true" />
+            <Link href="tel:+2348025726493">+2348025726493</Link>
+          </div>
+          <div className="flex justify-center items-center gap-x-3 bg-accent w-full p-5">
+            <EnvelopeIcon className="block h-12 w-12" aria-hidden="true" />
+            <Link href="mailto:info@awexlubricants.com">
+              info@awexlubricants.com
+            </Link>
+          </div>
+        </div>
+
         {/* Company Logo */}
         {/* <img
           src="/path-to-your-logo.png" // Replace with the actual path to your logo
@@ -34,47 +44,52 @@ const Footer = () => {
           style={{ maxWidth: '150px' }}
         /> */}
         <h4>Awex Oil Limited</h4>
-        <div className="flex flex-1 items-center justify-center sm:items-stretch">
-          <div className="hidden sm:ml-6 sm:flex sm:space-x-8">
-            {navLinks.map((navLink) => (
-              <a
-                key={navLink.name} // Add a unique key
-                href={navLink.href}
-                className="inline-flex items-center text-white hover:text-primary px-1 pt-1 text-sm font-medium text-gray-900"
-              >
-                {navLink.name}
-              </a>
-            ))}
+        <div className="">
+          <div className="my-10 flex flex-1 items-center justify-center sm:items-stretch">
+            <div className="sm:ml-6 sm:flex sm:space-x-8">
+              {navLinks.map((navLink) => (
+                <a
+                  key={navLink.name} // Add a unique key
+                  href={navLink.href}
+                  className={`inline-flex items-center hover:text-accent px-1 pt-1 text-sm font-medium text-gray-900 ${
+                    router.pathname === navLink.href
+                      ? 'text-accent'
+                      : 'text-white'
+                  }`}
+                >
+                  {navLink.name}
+                </a>
+              ))}
+            </div>
+          </div>
+
+          {/* Social Media Links */}
+          <div className="flex justify-center space-x-10">
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faFacebook} size="2xl" />
+            </a>
+            <a
+              href="https://twitter.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faLinkedin} size="2xl" />
+            </a>
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <FontAwesomeIcon icon={faInstagram} size="2xl" />
+            </a>
           </div>
         </div>
-
-        {/* Social Media Links */}
-        {/* Social Media Links */}
-        <div className="flex space-x-10 mt-10">
-          <a
-            href="https://facebook.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faFacebook} size="2xl" />
-          </a>
-          <a
-            href="https://twitter.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faLinkedin} size="2xl" />
-          </a>
-          <a
-            href="https://instagram.com"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <FontAwesomeIcon icon={faInstagram} size="2xl" />
-          </a>
-        </div>
       </div>
-      <div className="text-center bg-accent  mt-5 p-5">
+      <div className="text-center bg-primary mt-10 p-5 border-t-2 border-accent">
         © {new Date().getFullYear()} Awex Oil Limited. All Rights Reserved.
       </div>
     </footer>
