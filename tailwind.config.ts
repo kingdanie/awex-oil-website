7932f93d891383f634d20a5faf2b21914f11faae
king danie <daniedemola@gmail.com>
2023-08-23 15:36:55 +0100

    mordify font size of in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,6 +20,16 @@ const config: Config = {
         'primary-light': '#aa3219de',
       },
     },
+       fontSize: {
+      sm: '0.8rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '2.563rem',
+      '3xl': '2.953rem',
+      '4xl': '3.441rem',
+      '5xl': '4.052rem',
+      '6xl': '5.052rem',
+    }
   },
   plugins: [],
 };
