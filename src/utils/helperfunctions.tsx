51f069707a19fffdcbb24319bada5edb929c27f2
king danie <daniedemola@gmail.com>
2023-08-23 15:39:03 +0100

    added quote link to help functions

--- src/utils/helperfunctions.tsx
+++ src/utils/helperfunctions.tsx
@@ -9,4 +9,5 @@ export const navLinks = [
   { name: 'About', href: '/about-us' },
   { name: 'Services', href: '/services' },
   { name: 'Contact Us', href: '/contact-us' },
+  { name: 'Get Quote', href: 'tel:+2348025726493' },
 ];
