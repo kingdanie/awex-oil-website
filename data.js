59a3a59123f017050cbf3322bc1c1fa3216ffd58
king danie <daniedemola@gmail.com>
2023-09-04 19:53:58 +0100

    added more info to data layer

--- data.js
+++ data.js
@@ -1,4 +1,87 @@
+const exxyaOilImage = '/exxya-engine-oil.jpg'; // Replace with your image path
+const exxyaCoolantImage = '/exxya-coolant-image.jpg'; // Replace with your image path
+const exxyaGearOilImage = '/exxya-gear-oil-image.jpg';
+const ProdImage = '/14.jpeg'
+
 const AwexContent = {
+  About: {
+    Introduction: {
+      paragraphs: [
+        " At AWE.X Industries, we believe in reliably providing our customers with high-performance products at a price that's considered 'Value for Money.' In a world where technology is rapidly advancing, our commitment is to meet industry demands with technology-led products.",
+        "Our core value is giving customers 'Value for Money,' enabling them to stay competitive with our competitively priced products.",
+      ],
+      title: 'Reliable Performance at Great Value',
+      subTitle: 'Our Core Values',
+      imgSrc: '/14.jpeg',
+      altText: 'Awex Technologies',
+    },
+
+    Philosophy: {
+      title: 'Our Philosophy and Strengths',
+      paragraphs: [
+        '<strong>Innovation</strong> - We are constantly on the lookout for emerging technologies and new materials to stay ahead in the industry',
+        '<strong>Continuous Improvement</strong> - Our dedication to Research and Development drives continuous improvement in our products.',
+        "<strong>Flexibility</strong> - We're prompt and responsive in meeting customer needs, ensuring you get exactly what you require.",
+        '<strong>Forming Alliances with Customers</strong> - We understand and work closely with customers to provide customized services.',
+        '<strong>Total Customer Support</strong> - Our experienced team offers product selection expertise, product knowledge, training, and logistics support.',
+      ],
+    },
+
+    Quality: {
+      title: 'Consistent Quality',
+      paragraph:
+        "It's not enough to talk about quality; we consistently deliver it. As manufacturers of our own products, we control the inputs and processes, ensuring that our lubricants always meet the highest quality standards. We maintain ISO 9001 certification from the International Organization for Standardization as a testament to our commitment to quality control.",
+    },
+
+    Assurance: {
+      title: 'Quality Assurance',
+      paragraph:
+        "'We Blend It, We Back It' is more than a promise. Our EXxYA Oil products are manufactured using the highest-quality base oils and state-of-the-art additives. We consistently produce high-quality products and proudly stand behind them, offering peace of mind and support for your over-the-road or in-field needs.",
+    },
+
+    CustomSolutions: {
+      title: 'Custom ',
+      spantitle: 'Solutions',
+      paragraph:
+        "Sometimes, unique challenges require unique solutions. AWE.X's ability to blend lubricants in small batches allows us to create custom blends to meet your specific needs in the field, solving problems and preventing them from occurring.",
+    },
+
+    DealerOpportunities: {
+      title: 'Dealer ',
+      spantitle: 'Opportunities',
+      paragraph:
+        "Partner with us to add AWE.X Industries to your marketing portfolio. Our staff is ready to help you build more than just a product line; we're here to assist you in adding value to your customers through educational resources and exceptional support.",
+    },
+
+    OurMarketFocus: {
+      title: 'Our ',
+      spantitle: 'Market Focus',
+      paragraph:
+        'Our focus is centered on our distributors, customers, and business partners. We engage actively with the market, listening, observing, and learning. Our global perspective and daily commitment to execution ensure we satisfy and service the needs of our customers, suppliers, and partners. Together, we aim to penetrate new markets and expand our distribution channels for the future growth of our brand.',
+    },
+
+    MeetOurTeam: {
+      title: 'Meet ',
+      spantitle: 'Our Team',
+      paragraph:
+        'The AWE.X Industries management team brings extensive international lubricants experience gained at major oil companies. Our expertise covers product development, marketing, in-field lubricant technical support, stock planning, and logistics. We have a network of authorized distributors spanning many African countries and nationwide.',
+    },
+
+    OurAdaptiveStrategy: {
+      title: 'Our Adaptive Strategy',
+      spantitle: 'Solutions',
+      paragraph:
+        "Our adaptive strategy offers customized products to meet various market needs and requirements. In a constantly evolving lubricants market, we empower our customers with the flexibility to remain competitive. We equip our network with sales tools and keep them informed about product developments and information. AWE.X Industries' strategy revolves around fast adaptation to lubrication technology developments and customer needs, offering lubrication recommendations, oil test analysis, and condition monitoring support.",
+    },
+
+    JoinUs: {
+      title: 'Join Us',
+      spantitle: 'Solutions',
+      paragraph:
+        "Join us on our journey as we redefine the standards of value and quality in the lubricant industry. Together, we'll continue to drive progress, innovation, and excellence.",
+    },
+  },
+
   ExxyaDescription: {
     imgSrc: '/builder-batter-left.jpg',
     altText: 'exxya engine oil',
@@ -10,18 +93,26 @@ const AwexContent = {
       'Our products are manufactured from finest API stocks and state of the art additive technology to meet the most stringent requirements of APN SN, ACEA A5/B5 thus making it suitable for contemporary cars.',
     ],
   },
-
-  ExxyaDescription2: {
-    imgSrc: '/builder-batter-left.jpg',
-    altText: 'exxya engine oil',
-    subTitle: 'Ultra Superior Quality',
-    title: 'Designed for Modern Vehicles',
-    paragraphs: [
-      'EXXYA 5W 30 Fully Synthetic Engine Oil is a premium Engine Oil that offers superior quality and is specifically designed for modern cars running on petrol or diesel. This makes it suitable for a broad range of temperatures and one of the most commonly used oils in the Nigerian market.',
-      'EXXYA Oil has been formulated to clean away old sludge while preventing new sludge formation. No oil keeps engines cleaner than EXXYA 5W 30 Fully Synthetic Engine Oil; it outperforms conventional motor oil, reduces friction and wear at start-up, and protects your engine against performance-robbing sludge and varnish deposits, helping improve fuel economy.',
-      'Our products are manufactured from the finest API stocks and state-of-the-art additive technology to meet the most stringent requirements of API SN, ACEA A5/B5, thus making it suitable for contemporary cars.',
-    ],
-  },
+  products: [
+    {
+      name: 'EXxYA synthetic Engine Oil',
+      image: exxyaOilImage,
+      alt: 'EXxYA synthetic Engine Oil',
+      url: '/exxya-fully-synthetic-engine-oil',
+    },
+    {
+      name: 'EXxYA Coolant',
+      image: exxyaCoolantImage,
+      alt: 'EXxYA Coolant',
+      url: '#',
+    },
+    {
+      name: 'EXxYA Gear Oil',
+      image: exxyaGearOilImage,
+      alt: 'EXxYA Gear Oil',
+      url: '#',
+    },
+  ],
 };
 
 export default AwexContent;
