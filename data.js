bb62c496424db697464ddf5e103226f5a2ceb87f
king danie <daniedemola@gmail.com>
2023-09-04 11:15:53 +0100

    added data source

---
+++ data.js
@@ -0,0 +1,27 @@
+const AwexContent = {
+  ExxyaDescription: {
+    imgSrc: '/builder-batter-left.jpg',
+    altText: 'exxya engine oil',
+    subTitle: 'Ultra Superiour Quality',
+    title: 'Designed for Modern Vehicles',
+    paragraphs: [
+      'EXXYA 5W 30 Fully Synthetic Engine Oil is a premium Engine Oil that offers superior quality and specifically designed for modern cars running on petrol or Diesel. This makes it suitable for a broad range of temperatures, and one of the most commonly used oil in the Nigeria market.',
+      'EXXYA Oil has been formulated to clean away old sludge while preventing new sludge formation. No oil keeps engines cleaner then EXXYA 5W 30 Fully Synthetic Engine Oil, it outperforms conventional motor oil, reduces friction and wear at start-up and protects your engine against performance robbing sludge and varnish deposits and help improve fuel economy.',
+      'Our products are manufactured from finest API stocks and state of the art additive technology to meet the most stringent requirements of APN SN, ACEA A5/B5 thus making it suitable for contemporary cars.',
+    ],
+  },
+
+  ExxyaDescription2: {
+    imgSrc: '/builder-batter-left.jpg',
+    altText: 'exxya engine oil',
+    subTitle: 'Ultra Superior Quality',
+    title: 'Designed for Modern Vehicles',
+    paragraphs: [
+      'EXXYA 5W 30 Fully Synthetic Engine Oil is a premium Engine Oil that offers superior quality and is specifically designed for modern cars running on petrol or diesel. This makes it suitable for a broad range of temperatures and one of the most commonly used oils in the Nigerian market.',
+      'EXXYA Oil has been formulated to clean away old sludge while preventing new sludge formation. No oil keeps engines cleaner than EXXYA 5W 30 Fully Synthetic Engine Oil; it outperforms conventional motor oil, reduces friction and wear at start-up, and protects your engine against performance-robbing sludge and varnish deposits, helping improve fuel economy.',
+      'Our products are manufactured from the finest API stocks and state-of-the-art additive technology to meet the most stringent requirements of API SN, ACEA A5/B5, thus making it suitable for contemporary cars.',
+    ],
+  },
+};
+
+export default AwexContent;
