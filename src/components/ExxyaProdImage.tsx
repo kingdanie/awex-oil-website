b4fa15d59619fbac7158a69886958936271893cf
king danie <daniedemola@gmail.com>
2023-09-04 19:56:08 +0100

    added link to products

--- src/components/ExxyaProdImage.tsx
+++ src/components/ExxyaProdImage.tsx
@@ -1,25 +1,30 @@
+import Link from 'next/link';
 import React from 'react';
 
 const ExxyaProdImage = ({
   name,
   image,
   alt,
+  url,
 }: {
   name: string;
   image: string;
   alt: string;
+  url?: String
 }) => {
   return (
     <div className="flex flex-col relative">
-      <div className="flex-grow exxyaProductStyles exxyaproduct relative mb-4">
-        <img src={image} alt={alt} className="w-full h-full" />
-        <h3
-          style={{ position: 'absolute', bottom: '0px', color: '#fff' }}
-          className="text-lg p-6 font-semibold mb-2 text-white text-center"
-        >
-          {name}
-        </h3>
-      </div>
+      <Link href={`${url}`}>
+        <div className="flex-grow exxyaProductStyles exxyaproduct relative mb-4">
+          <img src={image} alt={alt} className="w-full h-full" />
+          <h3
+            style={{ position: 'absolute', bottom: '0px', color: '#fff' }}
+            className="text-lg p-6 font-semibold mb-2 text-white text-center"
+          >
+            {name}
+          </h3>
+        </div>
+      </Link>
     </div>
   );
 };
