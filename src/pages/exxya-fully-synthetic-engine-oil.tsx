2b6a04ac5a700ab892a0fb014980e56d528b8401
king danie <daniedemola@gmail.com>
2023-09-04 19:55:29 +0100

    exxya snthetic page 70% complete

--- src/pages/exxya-fully-synthetic-engine-oil.tsx
+++ src/pages/exxya-fully-synthetic-engine-oil.tsx
@@ -1,15 +1,21 @@
 import SectionWithRightImg from '@/components/SectionWithRightImg';
 import Layout from '@/components/layout';
 import AwexContent from '../../data';
+import WhyExxya from '@/components/WhyExxya';
 
 export default function ExxyaSyntheticOil() {
   return (
     <Layout>
-      <div>
-        <SectionWithRightImg
-          data={AwexContent.ExxyaDescription}
-        ></SectionWithRightImg>
-      </div>
+      <SectionWithRightImg
+        data={AwexContent.ExxyaDescription}
+      ></SectionWithRightImg>
+      <section className="text-primary flex flex-col justify-center items-center">
+        <h2 className="text-2xl md:text-4xl text-primary font-extrabold md:w-[60vw] text-center mb-10">
+          Why Choose{' '}
+          <span className="text-accent">EXxYA Synthetic Engine Oil?</span>
+        </h2>
+        <WhyExxya />
+      </section>
     </Layout>
   );
 }
