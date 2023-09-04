e723308b313edcdc499d09b60314d6e5febd6c82
king danie <daniedemola@gmail.com>
2023-09-04 12:06:27 +0100

    slugToHeader function

--- src/utils/helperfunctions.tsx
+++ src/utils/helperfunctions.tsx
@@ -12,3 +12,21 @@ export const navLinks = [
   { name: 'Contact Us', href: '/contact-us' },
   { name: 'Get Quote', href: 'tel:+2348025726493' },
 ];
+
+
+
+export function slugToTitle(slug: String) {
+  // Remove the leading slash, if it exists
+  const cleanedSlug = slug.startsWith('/') ? slug.slice(1) : slug;
+
+  // Split the slug by hyphens to separate words
+  const words = cleanedSlug.split('-');
+
+  // Capitalize the first letter of each word and join them with a space
+  const title = words
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(' ');
+
+  return title;
+}
+
