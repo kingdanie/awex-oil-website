b9c643f5aa1f9764eac47a57097917fe5f5e882d
king danie <daniedemola@gmail.com>
2023-09-06 18:54:56 +0100

    coming soon page

---
+++ src/pages/comingSoon.tsx
@@ -0,0 +1,19 @@
+import Layout from "@/components/layout"
+import React from 'react';
+
+
+export default function ComingSoon() {
+
+
+
+
+  return (
+    <>
+      <Layout>
+        <section className="text-primary">
+          <h2 className="text-4xl">Home page coming soon </h2>
+        </section>
+      </Layout>
+    </>
+  );
+}
