9fbf8c7b207123c2efd6a69e50ab6520b82d1181
king danie <daniedemola@gmail.com>
2023-09-26 12:02:47 +0100

    misc

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,11 +3,15 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Awex Industries Limited</title>
+        {/* <meta property="og:image" content={currentPhotoUrl} />
+        <meta name="twitter:image" content={currentPhotoUrl} /> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
