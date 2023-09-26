51a1293ed9ca85900196da407d39ed5b0fe479ac
king danie <daniedemola@gmail.com>
2023-09-26 12:02:13 +0100

    added an mage to gallery

---
+++ dataSource.js
@@ -0,0 +1,59 @@
+// dataImages.js
+
+export const dataSource = [
+  {
+    id: 1,
+    public_id: 'awex-oil-cover',
+    format: 'jpeg',
+  },
+  {
+    id: 2,
+    public_id: '12',
+    format: 'jpeg',
+  },
+  {
+    id: 3,
+    public_id: 'exxya-synth-oil',
+    format: 'png',
+  },
+  {
+    id: 4,
+    public_id: '5',
+    format: 'jpeg',
+  },
+  {
+    id: 5,
+    public_id: '7',
+    format: 'jpeg',
+  },
+  {
+    id: 6,
+    public_id: 'awex-oil-2560',
+    format: 'jpg',
+  },
+  {
+    id: 7,
+    public_id: '9',
+    format: 'jpeg',
+  },
+  {
+    id: 8,
+    public_id: '13',
+    format: 'jpeg',
+  },
+  {
+    id: 9,
+    public_id: '17',
+    format: 'jpeg',
+  },
+  {
+    id: 10,
+    public_id: 'Layer-2',
+    format: 'png',
+  },
+  {
+    id: 11,
+    public_id: 'exxya-oil-authorized',
+    format: 'jpg',
+  },
+];
