28d8e7060f6b634a4353637e4b2498600ce5d63c
king danie <daniedemola@gmail.com>
2023-09-26 12:10:40 +0100

    moved head seo schema to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -5,6 +5,11 @@ import Header from './header';
 const Layout = ({ children }: { children: any }) => {
   return (
     <main className="relative min-h-screen bg-gray-200 flex flex-col justify-between items-center max-w-full">
+      <Head>
+        <title>Awex Industries Limited</title>
+        {/* <meta property="og:image" content={currentPhotoUrl} />
+        <meta name="twitter:image" content={currentPhotoUrl} /> */}
+      </Head>
       <Header />
       <div className="min-h-[40vh]">{children}</div>
       <Footer />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,15 +3,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        <title>Awex Industries Limited</title>
-        {/* <meta property="og:image" content={currentPhotoUrl} />
-        <meta name="twitter:image" content={currentPhotoUrl} /> */}
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
