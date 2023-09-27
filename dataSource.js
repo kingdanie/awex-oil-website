8f18c93b8d25e13439d65f29e8d3905921112508
king danie <daniedemola@gmail.com>
2023-09-27 15:16:36 +0100

    fix gallery nav bug

--- dataSource.js
+++ dataSource.js
@@ -2,58 +2,81 @@
 
 export const dataSource = [
   {
-    id: 1,
-    public_id: 'awex-oil-cover',
-    format: 'jpeg',
-  },
-  {
-    id: 2,
-    public_id: '12',
-    format: 'jpeg',
-  },
-  {
-    id: 3,
+    id: 0,
     public_id: 'exxya-synth-oil',
     format: 'png',
   },
   {
-    id: 4,
-    public_id: '5',
+    id: 1,
+    public_id: '15',
+    format: 'jpg',
+  },
+  {
+    id: 2,
+    public_id: 'awex-oil-cover',
     format: 'jpeg',
   },
   {
+    id: 3,
+    public_id: 'exxya-oil-authorized',
+    format: 'jpg',
+  },
+  {
+    id: 4,
+    public_id: 'staff-standing-with-exxya-oil',
+    format: 'jpg',
+  },
+
+  {
     id: 5,
-    public_id: '7',
+    public_id: '12',
     format: 'jpeg',
   },
   {
     id: 6,
-    public_id: 'awex-oil-2560',
+    public_id: '11',
     format: 'jpg',
   },
   {
     id: 7,
-    public_id: '9',
+    public_id: '13',
     format: 'jpeg',
   },
+
   {
     id: 8,
-    public_id: '13',
+    public_id: '5',
     format: 'jpeg',
   },
   {
     id: 9,
-    public_id: '17',
+    public_id: '7',
     format: 'jpeg',
   },
   {
     id: 10,
-    public_id: 'Layer-2',
-    format: 'png',
+    public_id: '9',
+    format: 'jpeg',
   },
+
   {
     id: 11,
-    public_id: 'exxya-oil-authorized',
+    public_id: 'awex-oil-2560',
     format: 'jpg',
+  },
+  {
+    id: 12,
+    public_id: '10',
+    format: 'jpg',
+  },
+  {
+    id: 13,
+    public_id: '17',
+    format: 'jpeg',
+  },
+  {
+    id: 14,
+    public_id: 'Layer-2',
+    format: 'png',
   },
 ];
